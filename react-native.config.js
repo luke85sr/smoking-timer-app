1eeac70091a0c5a38e4501568b127f38126262e5
Luca Parisi <luke85sr@gmail.com>
2025-07-12 22:27:07 +0200

    fix: use packageName in react-native.config.js and add fallback in Codemagic

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   project: {
     android: {
-      androidPackageName: 'com.luke85sr.smokingtimer', // ✅ React Native 0.72 expects this key
+      packageName: 'com.luke85sr.smokingtimer', // 👈 compatibile con react-native CLI
     },
   },
 };
