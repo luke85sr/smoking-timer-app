6315f792af7676e201ba74ef335c778028f6e955
Luca Parisi <luke85sr@gmail.com>
2025-07-12 21:49:35 +0200

    fix: add androidPackageName for autolinking in bare workflow

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   project: {
     android: {
-      androidPackageName: 'com.luke85sr.smokingtimer', // <-- CAMBIATO QUI
+      androidPackageName: 'com.luke85sr.smokingtimer', // ✅ React Native 0.72 expects this key
     },
   },
 };
