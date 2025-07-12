fb0c1a62c7ce1c92b0e7466915dddb6b859761e2
Luca Parisi <luke85sr@gmail.com>
2025-07-12 21:38:20 +0200

    fix: rename packageName to androidPackageName for autolinking

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   project: {
     android: {
-      packageName: 'com.luke85sr.smokingtimer', // <-- il tuo package name
+      androidPackageName: 'com.luke85sr.smokingtimer', // <-- CAMBIATO QUI
     },
   },
 };
